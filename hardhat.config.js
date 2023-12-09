f2408dee9915ac0db8a4dc1e109d0917919b554e
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2023-12-10 04:05:45 +0530

    Added info for polygon mumbai network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/SsF4IfMulZTQTt-yuLWC38vPTZVf4m48",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.19",
 };
